ecd6b8e0985f570c810c0005c17dd24214b8cc70
mntk <kan@khvmntk.ru>
2019-09-18 10:30:37 +1000

    ФТ тест изменен что бы тестировать добавление нескольких элементов

--- functional_tests.py
+++ functional_tests.py
@@ -26,9 +26,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Купить павлиньи перья')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self. browser.find_element_by_id('id_list_table')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows), "Новый элемент списка не появился в таблице")
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
         self.fail('Закончить тест!')
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -8,13 +8,13 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     '''тест домашней страницы'''
 
-    def test_home_page_returns_correct_html(self):
-        '''тест: домашняя страница взвращает правильный html'''
+    def test_uses_home_template(self):
+        '''тест: используется домашний шаблон'''
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
