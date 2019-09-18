66bc56deca166b67f10e08421339332c55eec91e
mntk <kan@khvmntk.ru>
2019-09-19 08:46:10 +1000

    URL фдрес для 2 форм, перемещен код в представения

--- lists/views.py
+++ lists/views.py
@@ -3,13 +3,16 @@ from lists.models import Item
 
 def home_page(request):
     '''домашняя страница'''
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/odin-edinstvennii-spisok-v-mire/')
-
     return render(request, 'home.html')
+
 
 def view_list(request):
     '''представление списка'''
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    '''новый список'''
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/odin-edinstvennii-spisok-v-mire/')
