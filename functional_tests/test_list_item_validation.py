41158b909fbd642b31622d0b66ed6cbf77b40465
mntk <kan@khvmntk.ru>
2019-09-25 12:25:11 +1000

    Переименованы все Id и name в элементах Input елементов. По прежнему не работает

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,15 +8,15 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         '''тест: нельзя добавлять пустые элементы списка'''
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"
         ))
 
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Buy milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
@@ -26,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
             "You can't have an empty list item"
         ))
 
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.browser.get_item_input_box().send_keys('Make tea')
+        self.browser.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -16,7 +16,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_uses_home_template(self):
+    def test_home_page_uses_item_form(self):
         '''тест: домашняя страница использует форму для элемента'''
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
@@ -60,7 +60,7 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         self.client.post(
             f'/lists/{correct_list.id}/',
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
         )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -73,7 +73,7 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.post(
             f'/lists/{correct_list.id}/',
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
         )
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
@@ -82,7 +82,7 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         response = self.client.post(
             f'/lists/{list_.id}/',
-            data={'item_text': ''}
+            data={'text': ''}
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
@@ -93,7 +93,7 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -101,14 +101,14 @@ class NewListTest(TestCase):
 
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={'text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         '''тест: ошибки валидации отсылаются назад в шаблон
            домашней страницы'''
-        response = self.client.post('/lists/new', data={'item_text': ''})
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         expected_error= escape("You can't have an empty list item")
@@ -116,6 +116,6 @@ class NewListTest(TestCase):
 
     def test_invalid_list_items_arent_saved(self):
         '''тест: сохраняются недопустимые эелементы списка'''
-        self.client.post('/lists/new', data={'item_text': ''})
+        self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
