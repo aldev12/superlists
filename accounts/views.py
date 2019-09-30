6cf40e81234288ab06e78dac8ec0a5cff9053d24
mntk <kan@khvmntk.ru>
2019-09-30 15:29:09 +1000

    Добавлены тесты для проверки отправленного письма пользователю с сылкой для входа

---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
+
+from accounts.models import Token
+
+
+def send_login_email(request):
+    '''отправить сообщение для входа в систему'''
+    email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
+    send_mail(
+        'Your login link for Superlists',
+        message_body,
+        'noreply@superlists',
+        [email]
+    )
+    messages.success(
+        request,
+        "Проверьте свою почту, мы отправили Вам ссылку, которую можно использовать для входа на сайт."
+    )
+    return redirect('/')
+
+def login(request):
+#    user = auth.authenticate(uid=request.GET.get('token'))
+#    if user:
+#        auth.login(request, user)
+    return redirect('/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from accounts import urls as accounts_urls
 from lists import views as list_views
 from lists import urls as list_urls
 
@@ -6,4 +7,5 @@ from lists import urls as list_urls
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(accounts_urls)),
 ]
